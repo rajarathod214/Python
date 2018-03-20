6144a985d28b7ad8af6cf9569e50bcf693e3b728
km19pg19_rajiv_rathod <rathod.raja7@gmail.com>
2018-03-20 22:00:25 +0530

    added in my)_new

--- dict.py
+++ dict.py
@@ -3,3 +3,4 @@
 dict = {1:'r',2:'p',3:'q'}
 
 print dict.values()
+print dict.values()
--- file.py
+++ file.py
@@ -4,5 +4,7 @@ import sys
 
 print ('Number of Arguments:',len(sys.argv))
 print ('Argument list:',str(sys.argv))
+print ('Argument list:',str(sys.argv))
+print ('Argument list:',str(sys.argv))
 
 
